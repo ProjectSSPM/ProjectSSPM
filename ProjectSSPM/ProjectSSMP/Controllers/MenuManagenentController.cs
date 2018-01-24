17f386feb1c8a8f812b2d2b363091b08f2aa2d11
Palm <palm0767@github.com>
2018-01-24 10:48:01 +0700

    project update

---
+++ ProjectSSPM/ProjectSSMP/Controllers/MenuManagenentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectSSMP.Models;
+
+namespace ProjectSSMP.Controllers
+{
+    public class MenuManagenentController : BaseController
+    {
+        public MenuManagenentController(sspmContext context) => this.context = context;
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.userMenu = GetMenu();
+            var mm = await (from mg in context.MenuGroup select mg).ToListAsync();
+            return View(mm);
+        }
+        public IActionResult AddMenu()
+        {
+            ViewBag.userMenu = GetMenu();
+            return View();
+        }
+    }
+}
---
+++ ProjectSSPM/ProjectSSMP/Controllers/ProjectController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjectSSMP.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProjectSSMP.Controllers
+{
+    public class ProjectController : BaseController
+    {
+        // GET: /<controller>/
+        public ProjectController(sspmContext context) => this.context = context;
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            ViewBag.userMenu = GetMenu();
+            return View();
+        }
+    }
+}
