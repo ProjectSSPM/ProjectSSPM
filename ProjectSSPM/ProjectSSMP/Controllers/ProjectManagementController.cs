4aac34cb9629cd557431517324a13f5731cc477a
Palm <palm0767@github.com>
2018-01-25 11:24:02 +0700

    updatetoday

--- ProjectSSPM/ProjectSSMP/Controllers/ProjectManagementController.cs
+++ ProjectSSPM/ProjectSSMP/Controllers/ProjectManagementController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjectSSMP.Models;
 using ProjectSSMP.Models.ProjectManagement;
+using ProjectSSMP.Models.UserManagenent;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,13 +19,20 @@ namespace ProjectSSMP.Controllers
         public ProjectManagementController(sspmContext context) => this.context = context;
 
         [Authorize]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.userMenu = GetMenu();
+
+            return View(await context.Project.ToListAsync());
+        }
+
+        public IActionResult CreateProject()
         {
             ViewBag.userMenu = GetMenu();
             return View();
         }
 
-        public IActionResult CreateProject()
+        public IActionResult CreateAll()
         {
             ViewBag.userMenu = GetMenu();
             return View();
@@ -76,8 +85,8 @@ namespace ProjectSSMP.Controllers
 
 
                 var query = from xx in context.RunningNumber
-                                                  where xx.Type.Equals("ProjectNumber")
-                                              select xx;
+                            where xx.Type.Equals("ProjectNumber")
+                            select xx;
 
                 foreach (RunningNumber RunUserID in query)
                 {
@@ -107,5 +116,88 @@ namespace ProjectSSMP.Controllers
                 return View();
             }
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAll(CreateProjectInputModel inputModel)
+        {
+            ViewBag.userMenu = GetMenu();
+            try
+            {
+                var loggedInUser = HttpContext.User;
+                var loggedInUserName = loggedInUser.Identity.Name;
+                var id = (from u in context.RunningNumber where u.Type.Equals("ProjectNumber") select u).FirstOrDefault();
+
+                int num;
+                if (id.Number == null)
+                {
+                    num = 100001;
+
+                }
+                else
+                {
+                    num = Convert.ToInt32(id.Number);
+                    num = num + 1;
+                }
+
+                Project ord = new Project
+                {
+                    ProjectNumber = num.ToString(),
+                    ProjectId = inputModel.ProjectId,
+                    ProjectName = inputModel.ProjectName,
+                    ProjectStart = inputModel.ProjectStart,
+                    ProjectEnd = inputModel.ProjectEnd,
+                    ProjectCost = inputModel.ProjectCost,
+                    ProjectManager = inputModel.ProjectManager,
+                    ProjectCreateBy = loggedInUserName,
+                    ProjectCreateDate = DateTime.Now,
+                    CustomerName = inputModel.CustomerName,
+                    CustomerTel = inputModel.CustomerTel,
+                    Note = inputModel.Note
+
+                };
+
+
+                // Add the new object to the Orders collection.
+                context.Project.Add(ord);
+                await context.SaveChangesAsync();
+
+
+                var query = from xx in context.RunningNumber
+                            where xx.Type.Equals("ProjectNumber")
+                            select xx;
+
+                foreach (RunningNumber RunUserID in query)
+                {
+                    RunUserID.Number = num.ToString();
+
+                }
+
+                // Submit the changes to the database.
+                try
+                {
+                    await context.SaveChangesAsync();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    // Provide for exceptions.
+                }
+
+                return ViewBag.PID = num.ToString();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                var message = ex.Message;
+                return View();
+            }
+        }
+
     }
+
+
 }
+
---
+++ ProjectSSPM/ProjectSSMP/Models/ProjectManagement/IndexProjectModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ProjectSSMP.Models.ProjectManagement
+{
+    public class IndexProjectModel
+    {
+        public string ProjectNumber { get; set; }
+        public string ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectManager { get; set; }
+        public DateTime? ProjectStart { get; set; }
+        public DateTime? ProjectEnd { get; set; }
+        public int? ProjectCost { get; set; }
+        public string ProjectCreateBy { get; set; }
+        public DateTime? ProjectCreateDate { get; set; }
+        public string ProjectEditBy { get; set; }
+        public DateTime? ProjectEditDate { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerTel { get; set; }
+        public DateTime? ActualStart { get; set; }
+        public DateTime? ActualEnd { get; set; }
+        public string Note { get; set; }
+        public int? Variant { get; set; }
+        public string ProjectStatus { get; set; }
+    }
+}
