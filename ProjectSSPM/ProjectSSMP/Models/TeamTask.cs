272eb9c30b5eec0fd78bcfed960646354f9df06b
ilumi <godjara1995@gmail.com>
2018-01-30 10:07:33 +0700

    get database

--- ProjectSSPM/ProjectSSMP/Models/TeamTask.cs
+++ ProjectSSPM/ProjectSSMP/Models/TeamTask.cs
@@ -6,7 +6,7 @@ namespace ProjectSSMP.Models
     public partial class TeamTask
     {
         public string FunctionId { get; set; }
-        public string TeamId { get; set; }
+        public string UserId { get; set; }
         public string ProjectResponsible { get; set; }
         public string TaskId { get; set; }
         public string ProjectNumber { get; set; }
