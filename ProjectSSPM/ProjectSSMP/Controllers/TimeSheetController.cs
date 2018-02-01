6ea06fa1efe095faf82fcd53384571a5dc45c7ec
Palm <palm0767@github.com>
2018-02-01 14:06:39 +0700

    commmm

--- ProjectSSPM/ProjectSSMP/Controllers/TimeSheetController.cs
+++ ProjectSSPM/ProjectSSMP/Controllers/TimeSheetController.cs
@@ -6,7 +6,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjectSSMP.Models;
+using ProjectSSMP.Models.Timesheet;
 
 namespace ProjectSSMP.Controllers
 {
@@ -22,7 +24,53 @@ namespace ProjectSSMP.Controllers
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            var loggedInUser = HttpContext.User;
+            var loggedInUserName = loggedInUser.Identity.Name;
+            ViewBag.userMenu = GetMenu();
+
+            var checkID = context.UserSspm.SingleOrDefault(m => m.Username == loggedInUserName);
+            var checkTeamTask = context.TeamTask.SingleOrDefault(m => m.UserId == checkID.UserId);
+            //var checkProject = context.Project.SingleOrDefault(m => m.ProjectNumber == checkTeamTask.ProjectNumber);
+
+            var PJ = (from x in context.UserSspm
+                      join x2 in context.TeamTask on x.UserId equals x2.UserId
+                      join x3 in context.Project on x2.ProjectNumber equals x3.ProjectNumber
+                      where x.Username.Equals(loggedInUserName)
+                      select new
+                      {
+                          ProjectNumber = x3.ProjectNumber,
+                          ProjectName = x3.ProjectName,
+                          ProjectId = x3.ProjectId,
+                          Note = x3.Note,
+            });
+            /*
+            var e = new TimeSheetInputModel()
+            {
+                ProjectId = checkProject.ProjectId,
+                ProjectName = checkProject.ProjectName,
+                ProjectNumber = checkProject.ProjectNumber,
+                Note = checkProject.Note
+
+            };
+            */
+
+            List<TimeSheetInputModel> model = new List<TimeSheetInputModel>();
+
+
+
+            foreach (var item in PJ)
+            {
+
+                model.Add(new TimeSheetInputModel()
+                {
+                    ProjectId = item.ProjectId,
+                    ProjectName = item.ProjectName,
+                    ProjectNumber = item.ProjectNumber,
+                    Note = item.Note,
+
+                });
+            }
+                return View(model);
         }
     }
 }
---
+++ ProjectSSPM/ProjectSSMP/Models/Timesheet/TimeSheetInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ProjectSSMP.Models.Timesheet
+{
+    public class TimeSheetInputModel
+    {
+        public DateTime TimeSheetId { get; set; }
+        public string ActionId { get; set; }
+        public DateTime? TimeSheetStart { get; set; }
+        public DateTime? TimeSheetEnd { get; set; }
+        public string UserId { get; set; }
+        public string Firstname { get; set; }
+        public string FunctionId { get; set; }
+        public string FunctionName { get; set; }
+        public string TaskId { get; set; }
+        public string TaskName { get; set; }
+        public string ProjectNumber { get; set; }
+        public string ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string Note { get; set; } 
+    }
+}
